e5b583051b041687e492bbec5e56b596d33ceb8d
Sam <Sam@DESKTOP-IHGUGRL>
2021-07-22 14:34:39 +0100

    Add project files.

---
+++ ComputerShare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerShare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ComputerShare Coding Challenge");
+        }
+    }
+}
